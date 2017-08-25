ba901e833c3c6c483373e3a97f96aeb97a9c8e19
Simone Orsi <simahawk@gmail.com>
2017-08-25 10:48:37 +0200

    [add] dj_compilation_base (closes #48)

---
+++ dj_compilation_base/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'DJ set - Base compilation',
+    'summary': "Create stock songs from scratch",
+    'version': '10.0.1.0.0',
+    'author': 'Camptocamp,Odoo Community Association (OCA)',
+    'maintainer': 'Camptocamp',
+    'license': 'AGPL-3',
+    'category': 'songs',
+    'depends': ['base_dj', ],
+    'website': 'www.camptocamp.com',
+    'data': [
+        'data/compilation.xml',
+        'data/collection.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
